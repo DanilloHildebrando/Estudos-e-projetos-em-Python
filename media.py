1a18a937d9880925659ffe45ebe99d7ec894c065
DanilloHildebrando <105883654+DanilloHildebrando@users.noreply.github.com>
2022-09-07 19:01:52 -0300

    Add files via upload

---
+++ media.py
@@ -0,0 +1,17 @@
+'''
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+'''
+# Este algoritmo realiza o cálculo da média ponderada de 3 valores com pesos pré estabelecidos em 2, 3 e 5.
+
+num1 = int(input('Digite o primeiro valor: \n'))
+num2 = int(input('Digite o primeiro valor: \n'))
+num3 = int(input('Digite o primeiro valor: \n'))
+
+med = (num1 * 2 + num2 * 3 + num3 * 5) / 10
+
+print ("A média ponderada é:", med)
